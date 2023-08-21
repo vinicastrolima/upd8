2b7bf9f651aadbe4b12a306e17d1fb2384dbec40
Vinícius Castro <vinicastrolimavcl@gmail.com>
2023-08-21 01:17:35 -0300

    Feature => On controller, new functions to delete and update

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -82,20 +82,55 @@ class ClientesController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Cliente $clientes)
+    public function update(Request $request, Cliente $cliente)
     {
-        $cliente = Cliente::findOrFail($id);
-        $cliente->update($request->all());
+        // Validar os dados do request
+        $validator = Validator::make($request->all(), [
+            'cpf' => [
+                'required',
+                'string',
+                'size:11',
+                Rule::unique('clientes')->ignore($cliente->id),
+            ],
+            'nome' => 'required|string|max:100',
+            'data_nascimento' => 'required|date',
+            'sexo' => 'required|string|in:homem,mulher',
+            'endereco' => 'required|string|max:300',
+            'estado_id' => 'required|exists:estados,id',
+            'cidade_id' => 'required|exists:municipios,id',
+
+        ]);
+
+        // Se houver erros de validação, retornar as mensagens de erro
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        // Atualizar o cliente
+        $cliente->update([
+            'cpf' => $request->cpf,
+            'nome' => $request->nome,
+            'data_nascimento' => $request->data_nascimento,
+            'sexo' => $request->sexo,
+            'endereco' => $request->endereco,
+            'estado_id' => $request->estado_id,
+            'cidade_id' => $request->cidade_id,
+        ]);
+
         return response()->json($cliente, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy( $clientes)
+    public function destroy( $id)
     {
+        // Encontrar o cliente pelo ID
         $cliente = Cliente::findOrFail($id);
+
+        // Excluir o cliente
         $cliente->delete();
+
         return response()->json(null, 204);
     }
 }
